f3b9a72df2153c1cf1a97419846e7cd897e2f8ee
Aminjon Aliev <aminjon1065@gmail.com>
2022-09-24 12:11:09 +0500

    0.0.1

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -24,7 +24,7 @@ const Index = ({temp, error}) => {
             iconName: "smoke"
         },
         Haze: {
-            iconName: "day-haze\n"
+            iconName: "day-haze"
         },
         Dust: {
             iconName: "weather-windy"
